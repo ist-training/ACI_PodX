9fc3b055da0848aea7d9bee0e107c3b3211cb8a6
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-09 15:09:15 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aci_user" {
+  type = string
+  description = "the username for aci"
+}
+variable "aci_password" {
+  type = string
+  description = "The password for aci"
+}
+variable "aci_server" {
+  type = string
+  description = "the hostname or ip address of your APIC server"
+}
