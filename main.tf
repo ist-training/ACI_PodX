6800a44c08e3c5aa53c06dafb0cd79481e01a1db
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-10 10:39:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,25 @@ provider "ciscoasa" {
   ssl_no_verify = false
 }
 
+resource "ciscoasa_acl" "foo" {
+  name = "aclname"
+  rule {
+    source              = "192.168.10.5/32"
+    destination         = "192.168.15.0/25"
+    destination_service = "tcp/443"
+  }
+  rule {
+    source              = "192.168.10.0/24"
+    source_service      = "udp"
+    destination         = "192.168.15.6/32"
+    destination_service = "udp/53"
+  }
+  rule {
+    source              = "192.168.10.0/23"
+    destination         = "192.168.12.0/23"
+    destination_service = "icmp/0"
+  }
+}
 /*provider "aci" {
   username = var.aci_user
   password = var.aci_password
