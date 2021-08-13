6d3f00de84d3103870c1948888385fa89ef65605
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-13 10:37:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,56 @@ provider "aci" {
   insecure = true
 }
 
+module "aci_tenant" {
+  source  = "qzx/tenant/aci"
+  version = "1.0.0"
+
+  tenant_name = "IST_Tenant"
+  vrfs        = ["MY_VRF1", "MY_VRF2"]
+  bridge_domains = {
+    BD1 = {
+      routing = true
+      vrf     = "MY_VRF1"
+    },
+    BD2 = {
+      routing = false
+      vrf     = "MY_VRF2"
+    }
+  }
+  application_profiles = ["ONE", "TWO"]
+  epgs = {
+    EPG1 = {
+      application_profile = "ONE"
+      bridge_domain       = "BD1"
+      domains             = ["uni/phys-MY_PHYSICAL_DOMAIN"]
+      static_paths        = []
+    },
+    EPG2 = {
+      application_profile = "TWO"
+      bridge_domain       = "BD2"
+      domains             = ["uni/phys-MY_PHYSICAL_DOMAIN"]
+      static_paths = [
+        {
+          vlan_id = 100
+          path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
+        },
+        {
+          vlan_id = 100
+          path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
+        }
+      ]
+    }
+  }
+}
+
+
+
+/*
 resource "aci_tenant" "demotenant" {
   name        = "test_tf_tenant_podX"
   description = "This tenant is created by terraform ACI provider"
 }
-
+*/
 /*data "aci_tenant" "tenant_fetch" {
   name = "data_source_test"
 }*/
