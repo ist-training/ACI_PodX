652d2889e75397e0d3c8e59fe4b495f04a273a67
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-13 13:31:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,12 @@ variable "aci_url" {
   type = string
   description = "the hostname or ip address of your APIC server"
 }
+
+variable "tenant_name" {
+  type        = string
+  description = "The name of our new Tenant managed by Terraform"
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.tenant_name))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+  }
+}
