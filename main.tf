f1d7a03def2ee633edf51b6ac89ae782429f0e42
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-14 13:47:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aci" {
   url      = var.aci_url
   insecure = true
 }
-
+/*
 module "aci_tenant" {
   source  = "qzx/tenant/aci"
   version = "1.0.0"
@@ -55,26 +55,4 @@ module "aci_tenant" {
   }
 }
 
-
-
-/*
-resource "aci_tenant" "demotenant" {
-  name        = "test_tf_tenant_podX"
-  description = "This tenant is created by terraform ACI provider"
-}
 */
-/*data "aci_tenant" "tenant_fetch" {
-  name = "data_source_test"
-}*/
-
-
-/*
-output "tenant_dn" {
-  value = data.aci_tenant.tenant_fetch.id
-}
-output "app_dn" {
-  value = data.aci_application_profile.ap_fetch.id
-}
-output "epg_dn" {
-  value = data.aci_application_epg.epg_fetch.id
-}*/
