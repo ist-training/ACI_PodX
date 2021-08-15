14d7eb7b311f770c74c1f0cd647d14e10f25ebe1
ist-training <85614799+ist-training@users.noreply.github.com>
2021-08-15 12:51:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,26 +10,3 @@ variable "aci_url" {
   type = string
   description = "the hostname or ip address of your APIC server"
 }
-
-variable "tenant_name" {
-  type        = string
-  description = "The name of our new Tenant managed by Terraform"
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.tenant_name))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
-  }
-}
-
-variable "vrfs" {
-  type        = list(string)
-  description = "List of VRFs we want our new tenant to have"
-  default     = []
-
-  validation {
-    condition = alltrue([
-      for vrf in var.vrfs : can(regex("^[a-zA-Z0-9_.-]{0,64}$", vrf))
-    ])
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
-  }
-}
-
