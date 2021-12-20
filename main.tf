36543885f0ab25bd2ef03f4e95fc91f0ff2c7eea
ist-training <85614799+ist-training@users.noreply.github.com>
2021-12-20 12:03:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,11 +45,11 @@ module "aci_tenant" {
       domains             = ["uni/phys-MY_PHYSICAL_DOMAIN"]
       static_paths = [
         {
-          vlan_id = 111
+          vlan_id = 100
           path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
         },
         {
-          vlan_id = 222
+          vlan_id = 200
           path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
         }
       ]
